682fc7a7f453c297f58b1bb9556b95448b493d1c
Ihtesham Elahi <ihtesham.elahi.7@gmail.com>
2025-04-16 18:46:21 +0500

    New component

---
+++ 01react/src/Test.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function testing({ name }) {
+  return <h2> Testing something, new name is {name} </h2>;
+}
+export default testing;
--- 01react/src/main.jsx
+++ 01react/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import NewTest from "./Test.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <NewTest name="Ihtesham Elahi" />
   </StrictMode>
 );
