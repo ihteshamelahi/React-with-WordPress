20f7b2f9b717e792149c9dbd686ccc4e1b99fb25
Ihtesham Elahi <ihtesham.elahi.7@gmail.com>
2025-04-17 18:21:12 +0500

    Updated New

--- 01react/src/Test.jsx
+++ 01react/src/Test.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-function testing({ name }) {
-  return <h2> Testing something, new name is {name} </h2>;
+function testing({ name, Roll_No }) {
+  return (
+    <div>
+      <h4>Testing something, new name is {name}</h4>
+      <p>Roll No is {Roll_No}</p>
+    </div>
+  );
 }
 export default testing;
--- 01react/src/main.jsx
+++ 01react/src/main.jsx
@@ -6,6 +6,7 @@ import NewTest from "./Test.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <NewTest name="Ihtesham Elahi" />
+    <NewTest name="Ihtesham Elahi" Roll_No="51" />
+    <NewTest name="usama Maqsood" Roll_No="52" />
   </StrictMode>
 );
