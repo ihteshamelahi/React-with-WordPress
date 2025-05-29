9608b0687ce522f55fa9893f644de5b700d84fe4
Ihtesham Elahi <ihtesham.elahi.7@gmail.com>
2025-05-29 23:11:15 +0500

    Context Api Updated

---
+++ ContextAPI/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import UserContextProvider from "./context/UserContextProvider";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
